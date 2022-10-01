3709d564cd284fe139975904df3498189ea61115
Timotej Medved <timi.medved@gmail.com>
2022-10-01 12:42:15 +0200

    s

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
     path:"",
     domains: ['avatars.githubusercontent.com']
   },
-  basePath: "/nextjs-pages",
+  //basePath: "/nextjs-pages",
   assetPrefix: "/nextjs-pages",
 
   
